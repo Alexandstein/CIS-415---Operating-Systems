90315a353b29fd2f1f64dd335e26ffb44f652e3b
alexandstein <alexand.stein@gmail.com>
2014-06-23 03:12:57 -0700

    Read/Write mostly working

--- assign3/smsa_driver.c
+++ assign3/smsa_driver.c
@@ -19,6 +19,14 @@
 // Functional Prototypes
 
 ////////////////////////////////////////////////////////////
+//ERROR HELPER
+////////////////////////////////////////////////////////////
+
+void printErr(int errnum){
+	logMessage(1, smsa_error_string(errnum));
+}
+
+////////////////////////////////////////////////////////////
 //OP CODE HELPERS
 ////////////////////////////////////////////////////////////
 
@@ -64,6 +72,9 @@ int getDisk(SMSA_VIRTUAL_ADDRESS addr){
 int opSeekDisk(int drum){
 	uint32_t op = generateOP(SMSA_SEEK_DRUM, drum, 0);
 	int status = smsa_operation(op, NULL);
+	if(status){
+		printErr(status);
+	}
 	return status;
 }
 
@@ -71,6 +82,9 @@ int opSeekDisk(int drum){
 int opSeekBlock(int block){
 	uint32_t op = generateOP(SMSA_SEEK_BLOCK, 0, block);
 	int status = smsa_operation(op, NULL);
+	if(status){
+		printErr(status);
+	}
 	return status;
 }
 
@@ -81,7 +95,7 @@ int opSeek(SMSA_VIRTUAL_ADDRESS addr){
 	int status= 0;
 	uint32_t op = 0;
 	
-	logMessage(LOG_OUTPUT_LEVEL, "OP: Seek Disk: %d Block: %d (Addr=%d) ", disk, block, addr);
+	logMessage(16, "OP: Seek Disk: %d Block: %d (Addr=%d) ", disk, block, addr);
 	
 	opSeekDisk(disk);
 	opSeekBlock(block);
@@ -93,11 +107,11 @@ int opSeek(SMSA_VIRTUAL_ADDRESS addr){
 //READ/WRITE
 ////////////////////////////////////////////////////////////
 
-//Wrapper for writing
-//Negative address means do not change head position prior to writing
-int opWrite(int addr, char* buffer, int len){
-	if(addr < 0){					//Don't change head positions
-		logMessage(LOG_OUTPUT_LEVEL, "Write w/o Seek\n");
+//Wrapper for writing. Writes to a single block.
+//moveHead means to change head position to addr prior to writing
+int opWrite(SMSA_VIRTUAL_ADDRESS addr, unsigned char* buffer, int moveHead){
+	if(moveHead == 0){					//Don't change head positions
+		logMessage(16, "Write w/o Seek\n");
 	}else{							//Change head position
 		SMSA_VIRTUAL_ADDRESS address = addr;
 		opSeek(address);
@@ -105,12 +119,17 @@ int opWrite(int addr, char* buffer, int len){
 	
 	uint32_t op = generateOP(SMSA_DISK_WRITE, 0, 0);
 	int status = smsa_operation(op, buffer);
+	if(status){
+		printErr(status);
+	}
 	return status;
 }
 
-int opRead(int addr, char* buffer, int len){
-	if(addr < 0){					//Don't change head positions
-		logMessage(LOG_OUTPUT_LEVEL, "Write w/o Seek\n");
+//Read wrapper. Reads a single block.
+//moveHead means to change head position to addr prior to writing
+int opRead(SMSA_VIRTUAL_ADDRESS addr, unsigned char* buffer, int moveHead){
+	if(moveHead == 0){				//Don't change head positions
+		logMessage(16, "Write w/o Seek\n");
 	}else{							//Change head position
 		SMSA_VIRTUAL_ADDRESS address = addr;
 		opSeek(address);
@@ -118,24 +137,47 @@ int opRead(int addr, char* buffer, int len){
 	
 	uint32_t op = generateOP(SMSA_DISK_READ, 0, 0);
 	int status = smsa_operation(op, buffer);
+	if(status){
+		printErr(status);
+	}
 	return status;
 }
 
-
 //Wipe the contents of a block
-int formatBlock(SMSA_VIRTUAL_ADDRESS addr){
-	//TODO Everything
-	return -1;
+int formatBlockByAddr(SMSA_VIRTUAL_ADDRESS addr){
+	char* blank = calloc(SMSA_BLOCK_SIZE, sizeof(*blank));				//Make blank block
+	int status = opWrite(addr, blank, 1);
+	free(blank);
+	return status;
+}
+
+//Format entire SMSA disk
+int formatSMSA(){
+	int i;
+	int status;
+	disableLogLevels(4);
+	for(i = 0; i < MAX_SMSA_VIRTUAL_ADDRESS; i += SMSA_BLOCK_SIZE){
+		status = formatBlockByAddr(i);
+		if(status){										//Error check
+			printErr(status);
+			return status;
+		}
+	}
+	return 0;
 }
 
 //Store testing code
-int testing(){
-	int test = 256;
-	char* buffer = calloc(256, sizeof(char));
-	opWrite(test, "BLOOP\n", 6);
-	opRead(test, buffer, 6);
-	logMessage(LOG_OUTPUT_LEVEL, buffer);
-	
+void testing(){
+	enableLogLevels(16);
+	int test = 0;
+	unsigned char* buffer = calloc(SMSA_BLOCK_SIZE * 5, sizeof(*buffer));
+	unsigned char* input  = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+	unsigned char* input2 = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+	smsa_vwrite(test, 450, input);
+	smsa_vwrite(test + 1, 258, input2);
+	smsa_vread(test, 450, buffer);
+	logMessage(16, buffer);
+	free(buffer);
 }
 //
 // Global data
@@ -151,10 +193,23 @@ int testing(){
 // Outputs      : -1 if failure or 0 if successful
 
 int smsa_vmount( void ) {
+	registerLogLevel("DEBUG", 16);					//Register debugging
+	disableLogLevels(16 | 4);
+
 	uint32_t op = generateOP(SMSA_MOUNT, 0, 0);		//Mount disk
 	int status = smsa_operation(op, NULL);
+	if(status){										//Error check
+		printErr(status);
+		return -1;
+	}
 													//TODO Read data
-	testing();
+	status = formatSMSA();							//Format disk
+	if(status){										//Error check
+		printErr(status);
+		return -1;
+	}
+	testing();	return -1;		//DEBUGGING STUFF
+	return 0;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -169,6 +224,7 @@ int smsa_vunmount( void )  {
 	uint32_t op = generateOP(SMSA_UNMOUNT, 0, 0);		//Mount disk
 	int status = smsa_operation(op, NULL);
 														//TODO Save data
+	return 0;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -182,7 +238,36 @@ int smsa_vunmount( void )  {
 // Outputs      : -1 if failure or 0 if successful
 
 int smsa_vread( SMSA_VIRTUAL_ADDRESS addr, uint32_t len, unsigned char *buf ) {
+	logMessage(16, "Read of length: %d at addr %d\n", len, addr);
 	
+	
+	int i = 0;
+	int status = 0;
+	int offset = addr % SMSA_BLOCK_SIZE;
+	int offsetEnd = len + addr;
+	unsigned char* offsetBuffer;
+	
+	if(addr+len > MAX_SMSA_VIRTUAL_ADDRESS){			//Check if within bounds of addr	
+		logMessage(LOG_ERROR_LEVEL, "Out of Bounds Error. Attempted acces to addr: %d\n", len + addr);
+	}
+	int numOfBlocks = (offset + len - 1)/SMSA_BLOCK_SIZE + 1;	//Get number of blocks to read
+	offsetBuffer = calloc(numOfBlocks*SMSA_BLOCK_SIZE + 1, sizeof(*offsetBuffer));
+	
+	opSeek(addr);
+	for(i = 0; i < numOfBlocks; i++){
+		status = opRead(addr + i*SMSA_BLOCK_SIZE, offsetBuffer + i*SMSA_BLOCK_SIZE, 1);
+		if(status){
+			printErr(status);
+			return -1;
+		}
+	}
+	//TODO Allow non-block offsets
+	//Copy buffer over, but with offset
+	for(i = 0; i < len; i++){
+		buf[i] = offsetBuffer[i + offset];
+	}
+	
+	return 0;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -196,7 +281,41 @@ int smsa_vread( SMSA_VIRTUAL_ADDRESS addr, uint32_t len, unsigned char *buf ) {
 // Outputs      : -1 if failure or 0 if successful
 
 int smsa_vwrite( SMSA_VIRTUAL_ADDRESS addr, uint32_t len, unsigned char *buf )  {
-
+	int i = 0;
+	int status = 0;
+	int offset = addr % SMSA_BLOCK_SIZE;
+	int offsetEnd = len + addr;
+	unsigned char* offsetBuffer;
+	
+	if(addr+len > MAX_SMSA_VIRTUAL_ADDRESS){			//Check if within bounds of addr	
+		logMessage(LOG_ERROR_LEVEL, "Out of Bounds Error. Attempted acces to addr: %d\n", len + addr);
+	}
+	int numOfBlocks = (offset + len - 1)/SMSA_BLOCK_SIZE + 1;	//Get number of blocks to read
+	offsetBuffer = calloc(numOfBlocks*SMSA_BLOCK_SIZE + 1, sizeof(*offsetBuffer));
+	
+	opRead(addr, offsetBuffer, 1);			//Preload last and first blocks for splicing
+	opRead(addr + (numOfBlocks-2)*SMSA_BLOCK_SIZE, offsetBuffer, 1);			
+	
+	
+	//Copy buffer over, but with offset
+	for(i = 0; i < len; i++){
+		offsetBuffer[i + offset] = buf[i];
+	}
+	
+	logMessage(16, "Write of length: %d at addr %d (Target=%d\n", len, addr, len+addr);
+	opSeek(addr);
+	for(i = 0; i < numOfBlocks; i++){
+		status = opWrite(addr + i*SMSA_BLOCK_SIZE, offsetBuffer + i*SMSA_BLOCK_SIZE, 1); //Increment address
+		if(status){
+			printErr(status);
+			return -1;
+		}
+	}
+	//TODO Allow non-block offsets [X]
+	//TODO Read from file to offset buffer to splice last and first blocks on non-aligned writes[x]
+	//TODO Fix truncation problem at last blocks.
+	
+	return 0;
 }
 
-//logMessage(LOG_OUTPUT_LEVEL, "Test %d\n", 0);
+//logMessage(16, "Test %d\n", 0);
